27a464e86f4646b465bfed9181e0d9c72e6755d2
AntonBrodtkorb <56070612+AntonBrodtkorb@users.noreply.github.com>
2020-11-15 20:30:44 +0100

    adc_sample

---
+++ adc_sampl/adc_sampl.cydsn/adcsample.h
@@ -0,0 +1,21 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#ifndef adcsample_h
+
+#define adcsample_h
+
+void initADCsample();
+int getADCsample();
+
+#endif
+/* [] END OF FILE */
